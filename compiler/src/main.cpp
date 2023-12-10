29d4ee664d3ef0d87a9f49691102ee3734a219c6
ZXCqwe666 <76812116+ZXCqwe666@users.noreply.github.com>
2023-12-10 06:26:42 +0200

    c++ removed

--- compiler/src/main.cpp
+++
@@ -1,7 +0,0 @@
-import general;
-import cmd;
-
-i32 main(i32 argc, char** argv)
-{
-	return parse_cmd(argc, argv);
-}
--- premake5.lua
+++
@@ -1,8 +0,0 @@
-workspace "lang"
-    architecture "x64"
-    configurations { "debug", "dist" }
-    startproject "compiler"
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-include "compiler"
