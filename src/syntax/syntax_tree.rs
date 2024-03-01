7304d1dc5876255556568d467f868c008d858dd8
nickk-dv <76812116+nickk-dv@users.noreply.github.com>
2024-03-01 14:12:36 +0200

    lossless syntax tree with arcs

--- src/syntax/syntax_tree.rs
+++ src/syntax/syntax_tree.rs
@@ -1,4 +1,20 @@
-use std::iter;
+use std::{fmt, iter, sync::Arc};
+
+#[derive(Copy, Clone)]
+enum SyntaxKind {
+    // Nodes
+    Proc,
+    Name,
+    ParamList,
+    BinExpr,
+    // Tokens
+    Whitespace,
+    Ident,
+    ProcKw,
+    LitInt,
+    Plus,
+    Star,
+}
 
 #[derive(Copy, Clone)]
 enum NodeOrToken<N, T> {
@@ -6,26 +22,17 @@ enum NodeOrToken<N, T> {
     Token(T),
 }
 
-#[derive(Clone)]
-struct Node {
+type Node = Arc<NodeData>;
+struct NodeData {
     kind: SyntaxKind,
     children: Vec<NodeOrToken<Node, Token>>,
     len: usize,
 }
 
-#[derive(Clone)]
-struct Token {
+type Token = Arc<TokenData>;
+struct TokenData {
     kind: SyntaxKind,
     text: String,
-}
-
-#[derive(Copy, Clone)]
-enum SyntaxKind {
-    Proc,
-    ProcKw,
-    Name,
-    Ident,
-    ParamList,
 }
 
 impl NodeOrToken<Node, Token> {
@@ -43,10 +50,31 @@ impl NodeOrToken<Node, Token> {
     }
 }
 
-impl Node {
-    fn new(kind: SyntaxKind, children: Vec<NodeOrToken<Node, Token>>) -> Node {
+impl From<Node> for NodeOrToken<Node, Token> {
+    fn from(value: Node) -> Self {
+        Self::Node(value)
+    }
+}
+
+impl From<Token> for NodeOrToken<Node, Token> {
+    fn from(value: Token) -> Self {
+        Self::Token(value)
+    }
+}
+
+impl fmt::Display for NodeOrToken<Node, Token> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            NodeOrToken::Node(it) => fmt::Display::fmt(it, f),
+            NodeOrToken::Token(it) => fmt::Display::fmt(it, f),
+        }
+    }
+}
+
+impl NodeData {
+    fn new(kind: SyntaxKind, children: Vec<NodeOrToken<Node, Token>>) -> NodeData {
         let len = children.iter().map(|it| it.text_len()).sum();
-        Node {
+        NodeData {
             kind,
             children,
             len,
@@ -63,21 +91,30 @@ impl Node {
         self.children.as_slice()
     }
 
-    fn replace_child(&self, idx: usize, new_child: NodeOrToken<Node, Token>) -> Node {
+    fn replace_child(&self, idx: usize, new_child: NodeOrToken<Node, Token>) -> NodeData {
         assert!(idx < self.children.len());
-        let left_children = self.children.iter().take(idx).cloned();
-        let right_children = self.children.iter().take(idx + 1).cloned();
-        let new_children = left_children
+        let lhs_children = self.children.iter().take(idx).cloned();
+        let rhs_children = self.children.iter().skip(idx + 1).cloned();
+        let new_children = lhs_children
             .chain(iter::once(new_child))
-            .chain(right_children)
+            .chain(rhs_children)
             .collect();
-        Node::new(self.kind, new_children)
+        NodeData::new(self.kind, new_children)
     }
 }
 
-impl Token {
-    fn new(kind: SyntaxKind, text: String) -> Token {
-        Token { kind, text }
+impl fmt::Display for NodeData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for child in self.children() {
+            fmt::Display::fmt(child, f)?;
+        }
+        Ok(())
+    }
+}
+
+impl TokenData {
+    fn new(kind: SyntaxKind, text: String) -> TokenData {
+        TokenData { kind, text }
     }
 
     fn kind(&self) -> SyntaxKind {
@@ -89,4 +126,46 @@ impl Token {
     fn text_len(&self) -> usize {
         self.text.len()
     }
+}
+
+impl fmt::Display for TokenData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self.text(), f)
+    }
+}
+
+#[test]
+fn syntax_tree() {
+    let ws = Arc::new(TokenData::new(SyntaxKind::Whitespace, " ".to_string()));
+    let one = Arc::new(TokenData::new(SyntaxKind::LitInt, "1".to_string()));
+    let two = Arc::new(TokenData::new(SyntaxKind::LitInt, "2".to_string()));
+    let star = Arc::new(TokenData::new(SyntaxKind::Star, "*".to_string()));
+
+    let expr_mul = Arc::new(NodeData::new(
+        SyntaxKind::BinExpr,
+        vec![
+            one.into(),
+            ws.clone().into(),
+            star.into(),
+            ws.clone().into(),
+            two.into(),
+        ],
+    ));
+    // "1 + 2"
+    eprintln!("expr_mul: {}", expr_mul);
+
+    let plus = Arc::new(TokenData::new(SyntaxKind::Plus, "+".to_string()));
+
+    let expr_add = Arc::new(NodeData::new(
+        SyntaxKind::BinExpr,
+        vec![
+            expr_mul.clone().into(),
+            ws.clone().into(),
+            plus.into(),
+            ws.into(),
+            expr_mul.into(),
+        ],
+    ));
+    // "1 * 2 * 1 * 2"
+    eprintln!("expr_add: {}", expr_add);
 }
