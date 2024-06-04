e0860e7822d8aa692aebc994008518588d6beba2
nickk-dv <76812116+nickk-dv@users.noreply.github.com>
2024-06-04 12:45:42 +0300

    rock_cli format refactor

--- rock_cli/src/command/format.rs
+++ rock_cli/src/command/format.rs
@@ -12,26 +12,43 @@ pub fn parse() -> ResultComp<CommandFormat> {
     let mut p = FormatParser::new();
     let mut diagnostics = DiagnosticCollection::new();
 
-    let cmd_name = if let Some(name) = p.eat_arg() {
+    let format: CommandFormat = CommandFormat {
+        name: parse_name(&mut p, &mut diagnostics),
+        args: parse_args(&mut p),
+        options: parse_options(&mut p, &mut diagnostics),
+        trail_args: p.trail_args(),
+    };
+    ResultComp::new(format, diagnostics)
+}
+
+fn parse_name(p: &mut FormatParser, diagnostics: &mut DiagnosticCollection) -> String {
+    if let Some(name) = p.eat_arg() {
         name
     } else {
         diagnostics.error(ErrorComp::message(
             "command name is missing, use `rock help` to learn the usage",
         ));
         "error".into()
-    };
+    }
+}
 
+fn parse_args(p: &mut FormatParser) -> Vec<String> {
     let mut cmd_args = Vec::new();
+
     while let Some(arg) = p.eat_arg() {
         cmd_args.push(arg);
     }
+    cmd_args
+}
 
-    let mut duplicates = HashSet::new();
+fn parse_options(
+    p: &mut FormatParser,
+    diagnostics: &mut DiagnosticCollection,
+) -> HashMap<String, Vec<String>> {
     let mut cmd_options = HashMap::new();
+    let mut duplicates = HashSet::new();
 
-    while let Some(name) = p.eat_option() {
-        let opt_name = name;
-
+    while let Some(opt_name) = p.eat_option() {
         let mut opt_args = Vec::new();
         while let Some(arg) = p.eat_arg() {
             opt_args.push(arg);
@@ -44,64 +61,47 @@ pub fn parse() -> ResultComp<CommandFormat> {
         }
     }
 
-    // order of elements in hashmap gets random
     for duplicate in duplicates {
         diagnostics.warning(WarningComp::message(format!(
-            "duplicate options `--{duplicate}` will be ignored"
+            "duplicate `--{duplicate}` options will be ignored"
         )));
     }
-
-    let format = CommandFormat {
-        name: cmd_name,
-        args: cmd_args,
-        options: cmd_options,
-        trail_args: p.trail_args(),
-    };
-    ResultComp::new(format, diagnostics)
+    cmd_options
 }
 
 struct FormatParser {
-    args: Vec<String>,
     cursor: usize,
+    args: Vec<String>,
 }
 
 impl FormatParser {
     fn new() -> FormatParser {
         FormatParser {
-            args: std::env::args().skip(1).collect(),
             cursor: 0,
+            args: std::env::args().skip(1).collect(),
         }
     }
 
     fn eat_arg(&mut self) -> Option<String> {
-        match self.args.get(self.cursor) {
-            Some(arg) => {
-                if arg.starts_with("--") {
-                    None
-                } else {
-                    self.cursor += 1;
-                    Some(arg.clone())
-                }
-            }
-            None => None,
+        let arg = self.args.get(self.cursor)?;
+
+        if arg.starts_with("--") {
+            None
+        } else {
+            self.cursor += 1;
+            Some(arg.clone())
         }
     }
 
     fn eat_option(&mut self) -> Option<String> {
-        match self.args.get(self.cursor) {
-            Some(arg) => {
-                if let Some(option) = arg.strip_prefix("--") {
-                    self.cursor += 1;
-                    if option.is_empty() {
-                        None
-                    } else {
-                        Some(option.to_string())
-                    }
-                } else {
-                    None
-                }
-            }
-            None => None,
+        let arg = self.args.get(self.cursor)?;
+        let option = arg.strip_prefix("--")?;
+
+        self.cursor += 1;
+        if option.is_empty() {
+            None
+        } else {
+            Some(option.to_string())
         }
     }
 
