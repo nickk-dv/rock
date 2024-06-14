7f2086ce467e1c85c2470be0d65ab239e3ce4af2
nickk-dv <76812116+nickk-dv@users.noreply.github.com>
2024-06-15 01:14:44 +0300

    syntax tree design

--- rock_core/src/lib.rs
+++ rock_core/src/lib.rs
@@ -15,6 +15,7 @@ mod lexer;
 mod macros;
 pub mod package;
 pub mod session;
+mod syntax;
 mod temp_buffer;
 pub mod text;
 mod token;
---
+++ rock_core/src/syntax.rs
@@ -0,0 +1,93 @@
+use crate::arena::Arena;
+use crate::id_impl;
+use crate::token::token_list::TokenList;
+
+pub struct SyntaxTree<'syn> {
+    root: Node<'syn>,
+    arena: Arena<'syn>,
+    nodes: Vec<Node<'syn>>,
+    tokens: TokenList,
+}
+
+id_impl!(NodeID);
+pub struct Node<'syn> {
+    kind: SyntaxKind,
+    content: &'syn [NodeOrToken],
+}
+
+id_impl!(TokenID);
+pub enum NodeOrToken {
+    Node(NodeID),
+    Token(TokenID),
+}
+
+#[allow(non_camel_case_types)]
+pub enum SyntaxKind {
+    SOURCE_FILE,
+
+    PROC_ITEM,
+    PARAM_LIST,
+    PARAM,
+    ENUM_ITEM,
+    VARIANT_LIST,
+    VARIANT,
+    STRUCT_ITEM,
+    FIELD_LIST,
+    FIELD,
+    CONST_ITEM,
+    GLOBAL_ITEM,
+    IMPORT_ITEM,
+    IMPORT_SYMBOL_LIST,
+    IMPORT_SYMBOL,
+
+    VISIBILITY,
+    NAME,
+    NAME_REF,
+    ATTRIBUTE,
+    PATH,
+
+    TYPE_BASIC,
+    TYPE_CUSTOM,
+    TYPE_REFERENCE,
+    TYPE_PROCEDURE,
+    TYPE_ARRAY_SLICE,
+    TYPE_ARRAY_STATIC,
+
+    STMT_BREAK,
+    STMT_CONTINUE,
+    STMT_RETURN,
+    STMT_DEFER,
+    STMT_LOOP,
+    STMT_LOCAL,
+    STMT_ASSIGN,
+    STMT_EXPR_SEMI,
+    STMT_EXPR_TAIL,
+
+    EXPR_LIT_NULL,
+    EXPR_LIT_BOOL,
+    EXPR_LIT_INT,
+    EXPR_LIT_FLOAT,
+    EXPR_LIT_CHAR,
+    EXPR_LIT_STRING,
+    EXPR_IF,
+    EXPR_BLOCK,
+    STMT_LIST,
+    EXPR_MATCH,
+    MATCH_ARM_LIST,
+    MATCH_ARM,
+    EXPR_FIELD,
+    EXPR_INDEX,
+    EXPR_SLICE,
+    EXPR_CALL,
+    ARGUMENT_LIST,
+    EXPR_CAST,
+    EXPR_SIZEOF,
+    EXPR_ITEM,
+    EXPR_VARIANT,
+    EXPR_STRUCT_INIT,
+    EXPR_ARRAY_INIT,
+    EXPR_ARRAY_REPEAT,
+    EXPR_ADDRESS,
+    EXPR_UNARY,
+    EXPR_BINARY,
+}
